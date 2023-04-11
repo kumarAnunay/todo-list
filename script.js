ffed20ebfbc2db1f3f0481421198b59c21bbdae1
kumarAnunay <kumaranunay12@gmail.com>
2023-04-12 02:28:28 +0530

    add

--- script.js
+++ script.js
@@ -16,7 +16,20 @@ function todoHandler() {
     // console.log(user);
 }
 
+function resetData() {
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("number").value = "";
 
+}
+
+function displayList() {
+    let list = document.querySelectorAll(".list");
+    // debugger;
+    list[0].classList.remove("list");
+    list[1].classList.remove("list");
+}
 
 function appendData(user) {
 
@@ -27,15 +40,19 @@ function appendData(user) {
 
     let nameContainer = document.createElement("div");
     nameContainer.innerHTML = `Name:- ${user.name}`;
+    nameContainer.setAttribute("id", "nameUser");
 
     let ageContainer = document.createElement("div");
     ageContainer.innerHTML = `Age:- ${user.age}`;
+    nameContainer.setAttribute("id", "ageUser");
 
     let cityContainer = document.createElement("div");
     cityContainer.innerHTML = `City:- ${user.city}`;
+    nameContainer.setAttribute("id", "cityUser");
 
     let numberContainer = document.createElement("div");
     numberContainer.innerHTML = `Number:- ${user.number}`;
+    nameContainer.setAttribute("id", "numberUser");
 
     let btnContainer = document.createElement("div");
     btnContainer.classList.add("btnContainer");
@@ -45,13 +62,23 @@ function appendData(user) {
     deleteBtn.classList.add("deleteBtn");
     deleteBtn.addEventListener("click", (e) => {
         e.target.parentElement.parentElement.remove();
+
+        if(contentContainer.childNodes.length <= 1){
+            document.getElementById("header").classList.add("list");
+            contentContainer.classList.add("list");
+        }
     })
 
     let editBtn = document.createElement("button");
     editBtn.innerHTML = "Edit";
     editBtn.classList.add("editBtn");
     editBtn.addEventListener("click", (e) => {
-        editValues();
+        let user = editValues();
+        // debugger;
+        e.target.parentElement.parentElement.childNodes[0].innerText = user.name;
+        e.target.parentElement.parentElement.childNodes[1].innerText = user.age;
+        e.target.parentElement.parentElement.childNodes[2].innerText = user.city;
+        e.target.parentElement.parentElement.childNodes[3].innerText = user.number;
     });
 
     btnContainer.append(deleteBtn);
@@ -64,6 +91,9 @@ function appendData(user) {
     userContainer.append(btnContainer);
 
     contentContainer.append(userContainer);
+
+    resetData();
+    displayList();
 }
 
 function editValues() {
@@ -74,12 +104,13 @@ function editValues() {
     let updatedNumber = prompt("Enter correct Number");
 
     let user = {
-        name: updatedName,
-        age: updatedAge,
-        city: updatedCity,
-        number: updatedNumber
+        name: `Name:- ${updatedName}`,
+        age: `Age:- ${updatedAge}`,
+        city: `City:- ${updatedCity}`,
+        number: `Number:- ${updatedNumber}`
     }
-    appendData(user);
+
+    return user;
 
 }
 
