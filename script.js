0e6b158b1e6840c0c7d8876fb1347ad39e4bc607
kumarAnunay <kumaranunay12@gmail.com>
2023-05-03 23:40:48 +0530

    Add

--- script.js
+++ script.js
@@ -1,4 +1,124 @@
 
+// function todoHandler() {
+//     let user = {
+//         name: document.getElementById("name").value,
+//         age: document.getElementById("age").value,
+//         city: document.getElementById("city").value,
+//         number: document.getElementById("number").value
+//     }
+
+//     if (Object.values(user).includes("")) {
+//         alert("Please fill the required fields");
+//     }
+//     else {
+//         appendData(user);
+//     }
+//     // console.log(user);
+// }
+
+// function resetData() {
+//     document.getElementById("name").value = "";
+//     document.getElementById("age").value = "";
+//     document.getElementById("city").value = "";
+//     document.getElementById("number").value = "";
+
+// }
+
+// function displayList() {
+//     let list = document.querySelectorAll(".list");
+//     // debugger;
+//     list[0].classList.remove("list");
+//     list[1].classList.remove("list");
+// }
+
+// function appendData(user) {
+
+//     let contentContainer = document.getElementById("content");
+
+//     let userContainer = document.createElement("div");
+//     userContainer.classList.add("userContainer");
+
+//     let nameContainer = document.createElement("div");
+//     nameContainer.innerHTML = `Name:- ${user.name}`;
+//     nameContainer.setAttribute("id", "nameUser");
+
+//     let ageContainer = document.createElement("div");
+//     ageContainer.innerHTML = `Age:- ${user.age}`;
+//     nameContainer.setAttribute("id", "ageUser");
+
+//     let cityContainer = document.createElement("div");
+//     cityContainer.innerHTML = `City:- ${user.city}`;
+//     nameContainer.setAttribute("id", "cityUser");
+
+//     let numberContainer = document.createElement("div");
+//     numberContainer.innerHTML = `Number:- ${user.number}`;
+//     nameContainer.setAttribute("id", "numberUser");
+
+//     let btnContainer = document.createElement("div");
+//     btnContainer.classList.add("btnContainer");
+
+//     let deleteBtn = document.createElement("button");
+//     deleteBtn.innerHTML = "Delete";
+//     deleteBtn.classList.add("deleteBtn");
+//     deleteBtn.addEventListener("click", (e) => {
+//         e.target.parentElement.parentElement.remove();
+
+//         if(contentContainer.childNodes.length <= 1){
+//             document.getElementById("header").classList.add("list");
+//             contentContainer.classList.add("list");
+//         }
+//     })
+
+//     let editBtn = document.createElement("button");
+//     editBtn.innerHTML = "Edit";
+//     editBtn.classList.add("editBtn");
+//     editBtn.addEventListener("click", (e) => {
+//         let user = editValues();
+//         // debugger;
+//         e.target.parentElement.parentElement.childNodes[0].innerText = user.name;
+//         e.target.parentElement.parentElement.childNodes[1].innerText = user.age;
+//         e.target.parentElement.parentElement.childNodes[2].innerText = user.city;
+//         e.target.parentElement.parentElement.childNodes[3].innerText = user.number;
+//     });
+
+//     btnContainer.append(deleteBtn);
+//     btnContainer.append(editBtn);
+
+//     userContainer.append(nameContainer);
+//     userContainer.append(ageContainer);
+//     userContainer.append(cityContainer);
+//     userContainer.append(numberContainer);
+//     userContainer.append(btnContainer);
+
+//     contentContainer.append(userContainer);
+
+//     resetData();
+//     displayList();
+// }
+
+// function editValues() {
+
+//     let updatedName = prompt("Enter correct Name");
+//     let updatedAge = prompt("Enter correct Age");
+//     let updatedCity = prompt("Enter correct City");
+//     let updatedNumber = prompt("Enter correct Number");
+
+//     let user = {
+//         name: `Name:- ${updatedName}`,
+//         age: `Age:- ${updatedAge}`,
+//         city: `City:- ${updatedCity}`,
+//         number: `Number:- ${updatedNumber}`
+//     }
+
+//     return user;
+
+// }
+
+const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+const ageRegex = /^[1-9]\d*$/;
+const cityRegex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
+const numberRegex = /^\d{10}$/;
+
 function todoHandler() {
     let user = {
         name: document.getElementById("name").value,
@@ -7,8 +127,8 @@ function todoHandler() {
         number: document.getElementById("number").value
     }
 
-    if (Object.values(user).includes("")) {
-        alert("Please fill the required fields");
+    if (!nameRegex.test(user.name) || !ageRegex.test(user.age) || !cityRegex.test(user.city) || !numberRegex.test(user.number)) {
+        alert("Please fill the required fields correctly");
     }
     else {
         appendData(user);
@@ -63,7 +183,7 @@ function appendData(user) {
     deleteBtn.addEventListener("click", (e) => {
         e.target.parentElement.parentElement.remove();
 
-        if(contentContainer.childNodes.length <= 1){
+        if (contentContainer.childNodes.length <= 1) {
             document.getElementById("header").classList.add("list");
             contentContainer.classList.add("list");
         }
@@ -97,11 +217,25 @@ function appendData(user) {
 }
 
 function editValues() {
-
     let updatedName = prompt("Enter correct Name");
+    while (!nameRegex.test(updatedName)) {
+        updatedName = prompt("Invalid input. Please enter correct Name");
+    }
+
     let updatedAge = prompt("Enter correct Age");
+    while (!ageRegex.test(updatedAge)) {
+        updatedAge = prompt("Invalid input. Please enter correct Age");
+    }
+
     let updatedCity = prompt("Enter correct City");
+    while (!cityRegex.test(updatedCity)) {
+        updatedCity = prompt("Invalid input. Please enter correct City");
+    }
+
     let updatedNumber = prompt("Enter correct Number");
+    while (!numberRegex.test(updatedNumber)) {
+        updatedNumber = prompt("Invalid input. Please enter correct Number");
+    }
 
     let user = {
         name: `Name:- ${updatedName}`,
@@ -111,6 +245,6 @@ function editValues() {
     }
 
     return user;
-
 }
+
 
